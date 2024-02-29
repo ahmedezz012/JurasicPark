31c2203febb33a44a0e7a85fb979cb3d572ba993
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2024-03-01 01:22:52 +0200

    Initial Commit

---
+++ JP Apex predator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JP Apex predator
+//
+//  Created by Ahmed Ezz on 01/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JP Apex predator/JP_Apex_predatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_predatorApp.swift
+//  JP Apex predator
+//
+//  Created by Ahmed Ezz on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_predatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
